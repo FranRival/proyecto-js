837e033b4310fd1511c127f75171de0b9ec8d529
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-13 21:00:09 -0600

    2 tipos de variables

--- js/java.js
+++ js/java.js
@@ -1,2 +1,12 @@
 console.log("hello world")
-alert("hello")
+
+
+
+//anatomia de variables
+let variable = "numeros"
+
+//let es una caja, donde se almacenara. se almacena algo en la variable. la declaracion, y la asignacion, luego del =
+
+console.log(variable);
+
+const variableDos = 0 //el costs es para una caja que no cambiara alv
