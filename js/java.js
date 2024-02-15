7314fa87b58f7a390e607b9f838d6d0c41e0f62e
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-15 00:06:01 -0600

    tipo de dato primitivo. numeros, y tipo de presiciion de js

--- js/java.js
+++ js/java.js
@@ -1,43 +1,46 @@
 console.log("hello world")
 
-//string
-//primitivos y objetos
 
-const stringPrimitivo = 'un String primitivo'
-console.log(typeof stringPrimitivo)
+//dato primitivo, number 
+//1. tipo entero y decimal
 
 
-const stringPrimitivoTambien = String('un String primitivo')
-console.log(typeof stringPrimitivo)
+const entero = 32
+const decimal = 3.14
 
+console.log(typeof decimal, typeof entero) //no hay distincion
 
-//string primitivo - inmutables
+//2. notacion cientifica 
+const cientifico = 5e3
 
-const stringObjeto = new String('un String objeto')
-console.log(typeof stringObjeto) //sale un objeto 
+//3. numero infinitos y no numeros 
+const inifinito = Infinity
+const noEsUnNumero = NaN
 
-const saludo = 'Hola, Como estas?'
-console.log(saludo[0]) //en el indice, arroja la letra
-console.log(saludo.charAt(2))//acceder a los indices en el string. 
-console.log(saludo.indexOf('C')) //cuando no sabemos en que ubicacion esta la letra
-console.log(saludo.indexOf('estas')) //desde donde comienza la palabra
-console.log(saludo.indexOf('estamos'))//acceder a una que no existe. -1. 
-console.log(saludo.lastIndexOf('s')) //la ultima pocision de la ultima letra
-console.log(saludo.slice(3,5)) //la ubicacion de un punto, a otro, regresa las letras dentro
-console.log(saludo.length) //cantidad de caracteres
-console.log(saludo.toLocaleUpperCase()) //mayusculas
-console.log(saludo.toLocaleLowerCase())//minusculas
+//4.operaciones aritmeticas.
+//--suma, resta, mutiplicacion y division 
 
-const saludoDividido = saludo.split(' ')
-console.log(saludoDividido) //dividido por espacios
-console.log(saludoDividido[2]) //el resultado, .estas?, porque fue divididio desde arriba
+const suma = 3 + 4
+const resta = 7 - 4
+const multiplicacion = 5 * 4
+const division = 7 / 4
 
-const saludoConEspacios = ' Hello ahora '
-const saludosSinEspacio = saludoConEspacios.trim()
-console.log(saludosSinEspacio) //quita los esapcios al principio y al final 
+//--modulo y exponenciacion 
+const modulo = 14 % 4 //saber si un numero es multiplo de otro numero
+const exponenciacion = 2 ** 3 //potencia
 
-const saludoOriginal = 'Hellooooo world '
-const nuevoSaludo = saludoOriginal.replace('world', '🌚')
-console.log(nuevoSaludo) //reemplazando palabras
+//--precisiion
+const resultado = 0.1 + 0.2
+console.log(resultado) //dilema de preicision
+console.log(resultado.toFixed(1)) //error de precision arreglado
+console.log(resultado === 0.3)//para comprobar a nivel de typo. javascript no es precisio a este nivel. 
 
+//math operaciones avanzadas
+const raizCuadrada =  Math.sqrt(8)
+const valorAbsoluto = Math.abs(9)
+const numeroRandom = Math.random()
+
+console.log("los resultados de las operaciones" + raizCuadrada)
+console.log(valorAbsoluto)
+console.log(numeroRandom)
 
