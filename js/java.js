1c3f760a51d005a2527a0dbf1f8958861fff8303
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-16 15:48:04 -0600

    conversion explicita e implicita, cuando eucuentra un string, concatena, cuando no hay string, suma

--- js/java.js
+++ js/java.js
@@ -1,35 +1,57 @@
 console.log("hello world")
 
-//type castign y coercion: conversion de tipos
-//lenguajes compilados: c, c++, rust, go, swift: este codigo es traducido antes de la ejecucion
-//chequeo estatico detipos 
+//conversion de tipos explicita e implicita 
 
-//interpretados: se traducen confirme se traduce el codigo 
-//chequeto dinamico de tipos 
+//explicita type casting
 
-//js no se da cuenta, hasta que va revisando el programa
+const stringNumero = '42'
+const integer = parseInt(stringNumero)
+console.log(integer);
+console.log(typeof integer) //convirtio a tipo de dato numero
 
-//js tiene un tipado debil: 
+//conversion a flotante
 
-//una variable tiene un valor numero, y luego volver a asignarle un valor string. eso lo permite js
+const stringDecimal = '3.14'
+const float = parseFloat (stringDecimal)
+console.log(float)
+console.log(typeof float)
 
-const saludo = 'Hola'
-console.log(saludo)
+//conversion a binario
 
-const despedida = 'ADios'
-console.log(despedida)
+const binary = '1010'
+const decimal = parseInt(binary, 2)
+console.log(decimal)//solo toma 2 numeros. 
+console.log(typeof decimal)
 
-const titulo = 'No me gusta la sopa'
-console.log(titulo) //aqui ocurre un error? 
-
-const descripcion = 'porque esta caliente'
-console.log(descripcion)
+//implicit type casting
+const sum = '5' + 3
+console.log(sum) //una concatenacion, convierte el 3 en string
 
 
-const numero = 2
-const booleano = true
-console.log(numero+booleano) //error: sale 3, los covierte a conveniencia de js 
+const sumWithBoolean = '3' + true
+console.log(sumWithBoolean) //vio el string, lo conviertio en string, e hizo la concatenacion 
 
-//CONVERSION
-//implicita: 2 + true
-//explicita: string(), Numbre(), Boolean()
+//en que caso suma y concatena
+const sumWithNumber = 2 + true 
+console.log(sumWithNumber)
+
+
+
+const stringValue = '10'
+const numberValue = 10
+const booleanValue = true
+
+
+console.log('----------');
+console.log(stringValue+stringValue) //concaenar 
+console.log(numberValue+numberValue) // sumar
+console.log(booleanValue+booleanValue) // sumar
+console.log(numberValue+stringValue)//concatenar
+console.log(numberValue+booleanValue) //sumar
+console.log(booleanValue+stringValue)//concatenar
+console.log(booleanValue+numberValue)//sumar 
+console.log(booleanValue+booleanValue)//sumar 
+
+
+//si ecuentra un string, concatena
+//cuando es sin string, suma 
