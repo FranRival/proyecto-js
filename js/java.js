24d5a605f15a2be05c28b00a8ee85d2d02a331ec
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-16 16:01:55 -0600

    uso de comillas literarias

--- js/java.js
+++ js/java.js
@@ -1,10 +1,31 @@
 console.log("hello world")
 
-const numero = '596'
-const numerpConvertido = parseInt(numero)
 
-console.log(numero)
-console.log(typeof numerpConvertido) 
+//social media profile
 
-/* el resultado es string y numero  */
+//1. usar informacion
+const username = 'codingAdventure'
+const fullName = 'Jon Doe'
+const age = 25
+const isStudent = true
 
+//2. adress
+const adress = {
+    street: '123 main street',
+    city: 'tecville',
+    state: 'Codignlane',
+    codezip: 54321
+}
+
+const hobbies = ['COding', 'Reading', 'Gaming']
+
+//texto personaliado. Generatoing, personalized bio
+const personalizedBio = `Hola, soy ${fullName}. 
+Tengo ${age} years old. 
+y vivo en ${adress.city},
+I love ${hobbies.join(', ')}.
+Followme for more codding adventures. 
+` 
+
+//Print the use profile and bio
+console.log(personalizedBio)
