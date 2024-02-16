ebed5b948987948cbdb1d1e7a09ca0b7ca18ab79
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-16 14:58:55 -0600

    booleanos, y la declaracion implicita

--- js/java.js
+++ js/java.js
@@ -1,2 +1,27 @@
 console.log("hello world")
 
+//conversion a booleanos 
+
+const isActive = true
+const hasPermission = false 
+
+
+//conversion implicita 
+const resultado = 5 > 3
+
+console.log(resultado) //el resultado, muestra true o false 
+
+
+const name = "Javsascript"
+console.log(!!name) //para saber si existe la variable y si tiene algun valor 
+
+
+//conversion explicita
+const value = 0
+const explicitBoolean = Boolean(value)
+console.log(explicitBoolean) //toma el 0 con falso
+
+const otherValie = 6
+const explicitBoolean1 = Boolean(otherValie)
+console.log(explicitBoolean1)
+
