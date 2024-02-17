23e41fe6aa8dd912607b7bc2106eeea9f2170da1
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-17 17:38:51 -0600

    identificador this parte 1

--- js/java.js
+++ js/java.js
@@ -1,60 +1,36 @@
 console.log("hello world")
 
+//identificador this. contexto de ejcucion. 
 
-//funciones puras e impuras alv
+//casa = objeto
+//letero = this
+//perro = funcion
 
-//puras
-//misma entrada, misma salida. no producen efectos secundarios. 
+const house = {
+    dogName: 'Fido',
+    dogGreeting: function (){
+        console.log(`Hellooo iam ${this.dogName}`)
+    } 
+}
 
-//side effects para que dejen de ser puras 
-//1. modificar variables globales: 
-//2. modificar parametros 
-//3. solicitudes http
-//4. todo lo que sea imprimir mensages en pantalla o consola 
-//5. manipulacion del dom
-//6. obtener la hora actual 
+house.dogGreeting()
 
-
-function sum (a,b){
-    return a +b
-} //funcion pura: recibimos los mismos parametros, y obtenedremos la misma salida 
+//objeto: house, ese objeto tiene caracteristicas, nombre y saludo 
+//queremos acceder a propiedade de ese mismo objeto.
+//ese this es alusivo al objeto al que esta vinculado.
+//se le conoce como enlace implicite - implicit binding
 
 
-//funcion ya no es pura, es impura
-function sum (a,b){
-    console.log('A ', b);
-    return a +b
+function dogGreeting(){
+    console.log(`Hellooo iam ${this.dogName}`)
 } 
 
-let total = 0 
-
-function sumWithSideEffect (a){
-    total += a //+= nos ahorramos escribir otra vez la palabra 
-    return total
-}//funciones que son impuras 
-
-
-
-
-
-
-//funcion pura
-function square(x){
-    return x * x
+const house1 = {
+    dogName: 'Fido',
+    Age: 3
+     
 }
 
+house1.dogGreeting() // sale un error. porque el this ya no apunta
 
-//funcion pura 
-function addTen (y){
-    return y + 10
-}
-
-const number = 5
-const finalResult = addTen(square(number))//coomvinacion de 2 funciones puras
-
-console.log(finalResult) //sale 35
-
-//wai? 
-//square: 5 * 5 = 25
-//addteen: son 10, porque y mas 10, = 10.
-
+//anclar o vincular con ese objeto. en el siguiente commit
