fbd95c13f832d721a39964015a441b1096d34e59
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-17 16:38:34 -0600

    funciones vs metodos parte 2

--- js/java.js
+++ js/java.js
@@ -1,51 +1,5 @@
 console.log("hello world")
 
-//funciones vs methodos 
-
-//capacidades que tienen las funciones al igual que otros objetos 
-
-//1. Pasar funciones como argumentos  -> callback 
-
-
-function a (){}
-
-function b (a){}
-
-b(a)
-
-//retornar funciones 
-
-
-function a (){ //declaracion de funcion. 
-    function b (){}
-    return b
-}
-
-//asignar funciones a variables
-
-const a = function (){} //es como si esta funcion tuviera esta variable. -> expresion de funcion.
-
-//tener propiedades y metodos 
-
-function d (){}
-const obj = {}
-d.call(obj) //su contexto de ejecucion. las funciones tienen metodos asociados. 
-
-//anidar funciones -> nester functions 
-
-
-function a (){ //funciones anidadas 
-    function b (){
-        function c (){
-        }
-        c()
-    }
-    b()
-}
-
-
-//es posible almacenar funciones en objetos? 
-
 
 const rocket = {
     name: 'Falcon 9',
@@ -55,3 +9,5 @@ const rocket = {
 }
 
 rocket.launchMessage()
+
+//si es posible alamcenar funciones en objetos 
