03738311ec1a7da6e06f3c4ecb4a0b9e8bd461b9
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-18 15:01:07 -0600

    con mensaje personalizado

--- js/java.js
+++ js/java.js
@@ -2,15 +2,15 @@ console.log("hello world")
 
 
 //funciones constructoras
-function Rocket (name){// crear un objeto a partir de una funcion. y creamos sus priedades.
+function Rocket (name, ownedMessage){// crear un objeto a partir de una funcion. y creamos sus priedades.
     this.name = name
     this.launchMessage = function (){
-        console.log('🌅')
+        console.log(ownedMessage)
     }
 }
 
-const falcon9Rocket = new Rocket('Falcon9') //una instancia de ese objeto
-const falcon10Rocket = new Rocket('Falcon10')
+const falcon9Rocket = new Rocket('Falcon9', 'la gorda esta en el tobogan') //una instancia de ese objeto
+const falcon10Rocket = new Rocket('Falcon10', 'a ver si no se estrella')
 
 
 console.log(falcon9Rocket.name)
