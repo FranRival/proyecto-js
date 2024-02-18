a2f504236b6dec8b5f1d161436228332099dc282
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-18 15:12:31 -0600

    como crear objetos a partir de funciones de js, y se hacen con funciones constructoras,y tambien,,, crear objetos con arrow funcion sin utilizar new

--- js/java.js
+++ js/java.js
@@ -17,4 +17,16 @@ const falcon10Rocket = new Rocket('Falcon10', 'a ver si no se estrella')
 console.log(falcon9Rocket.name)
 console.log(falcon9Rocket.launchMessage())
 
+//crear funciones constructrasa partir de arrow functions. no se puede utilizar new.
 
+const RocketWithArrowFunction = (name, ownedMessage) => ({ //objeto a apartir de un arrow function
+    name: name,
+    launchMessage: ownedMessage
+})
+
+const personalizedMessageForArrowFunction = () => 'xxxxxxxxx!'
+const falcon9Rocket1 = RocketWithArrowFunction('Falcon99', personalizedMessageForArrowFunction)
+
+console.log(falcon9Rocket1.name)
+console.log(falcon9Rocket1.launchMessage) //aparece la funcion, 
+console.log(falcon9Rocket1.launchMessage())//aparece ejecutada la funcion 
