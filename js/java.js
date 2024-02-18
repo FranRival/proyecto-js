e7b1ec955a115628c02e163b96eb47aa4a9c0af8
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-18 14:52:37 -0600

    funciones constructoras parte1

--- js/java.js
+++ js/java.js
@@ -1,57 +1,17 @@
 console.log("hello world")
 
-//funciones de flecha y enlace lexico
 
-//funcion tradicional
+//funciones constructoras
 
-function almuerzo (proteina,verdura){
-    return `${proteina}, ${verdura}`
+const cohete = {
+    nombre: 'Falcon 9',
+    mensajeDespegue: function mensageDeDespegue (){
+        console.log('🌀');
+    }
 }
 
-almuerzo ('🥩','🌵')
+//duplicar codigo: es dificil, 
+// escribir un monton de cohetes es dificil, son un monton de cohetes.
 
 
-
-//arrow function
-
-const almuerzo1 = (proteina,verdura)=>{
-    return `${proteina}, ${verdura}`
-}
-
-
-//ejemplo vivo de un arrow functiom
-
-const greeting = function (nombre){
-    return`Hello,, ${nombre}`
-}
-
-
-//arrow function - explicit return
-const newGreeting = (nombre) => { //misma expresionde funcion pero sin function y con flecha
-    return`Hello,, ${nombre}`
-}
-
-//arrow function - implicit return
-const newGreetingImplicit = nombre => `Hello,, ${nombre}`
-
-//necesitamos colocar los parentesis cuando hay mas de un parametro
-
-const newGreetingImplicitWithTwoParameters = (nombre, lastName) => `Hello,, ${nombre}, ${lastName}`
-
-
-
-//lexical binding
-const ficcionalCaraceter = { //tenemos un objeto
-    name: 'Uncle jack', //nombre
-    messageWithTradicionalFunction: function (message){//metodo
-        console.log(`${this.name} say: ${message}`)
-    },
-    messageWithArrowfunction: (message)=> {
-        console.log(`${this.name} say: ${message}`)
-    },
-}
-
-
-ficcionalCaraceter.messageWithTradicionalFunction('El tio ben se le dispararon alv') //aqui toma el nombre
-ficcionalCaraceter.messageWithArrowfunction('ya esta tieso y spiderman los see') //aqui no toma nada del nombre. this para arrow, no existe. 
-
+//funciones sirven para reutilizar codigo. crear fragmentos de codigo, y con esos fragmentos crear algo mas. 
