02519814aed7a63efb22430c5d90de9119ee9709
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-18 15:31:16 -0600

    tipos de binding

--- js/java.js
+++ js/java.js
@@ -1,12 +1,60 @@
 console.log("hello world")
 
 
-'use strict'
-window.name = 'SpongeBob'
+//implicit binding
+const person = {
+    name: 'Adam',
+    greet: function() {
+      console.log(`Hello, I'm ${this.name}`);
+    }
+  };
+  
+  person.greet(); // Output: Hello, I'm Adam
 
 
-function fevoritCaracter (){
-    console.log(this.name)
-}
 
-fevoritCaracter()
+
+  //Explicit binding
+  function greet() {
+    console.log(`Hello, I'm ${this.name}`);
+  }
+  
+  const adam = { name: 'Adam' };
+  
+  greet.call(adam); // Output: Hello, I'm Adam
+
+
+
+  //New Binding
+
+  function Person(name) {
+    this.name = name;
+  }
+  
+  const adam1 = new Person('Adam');
+  console.log(adam1.name); // Output: Adam
+
+
+
+  //Lexical Binding:
+
+  const person1 = {
+    name: 'Adam',
+    greet: function() {
+      const innerFunction = () => {
+        console.log(`Hello, I'm ${this.name}`);
+      };
+      innerFunction();
+    }
+  };
+  
+  person1.greet(); // Output: Hello, I'm Adam
+
+
+  //Window Binding:
+  function showName() {
+    console.log(this.name);
+  }
+  
+  window.name = 'Adam';
+  showName(); // Output: Adam
