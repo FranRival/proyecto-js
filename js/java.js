9af7b00541ff94e4b3f5a1ab638c105ddfe4ba28
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-18 15:05:19 -0600

    una funcion con mensajes personalizados

--- js/java.js
+++ js/java.js
@@ -1,15 +1,16 @@
 console.log("hello world")
 
 
+
+const personalizedMessage = () => 'la gorda esta en el tobogan!'
+
 //funciones constructoras
 function Rocket (name, ownedMessage){// crear un objeto a partir de una funcion. y creamos sus priedades.
     this.name = name
-    this.launchMessage = function (){
-        console.log(ownedMessage)
-    }
+    this.launchMessage = ownedMessage 
 }
 
-const falcon9Rocket = new Rocket('Falcon9', 'la gorda esta en el tobogan') //una instancia de ese objeto
+const falcon9Rocket = new Rocket('Falcon9', personalizedMessage) //una instancia de ese objeto
 const falcon10Rocket = new Rocket('Falcon10', 'a ver si no se estrella')
 
 
