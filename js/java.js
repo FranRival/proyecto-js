931e0b10afaedd9bb7ac799253d23d84d14a0f13
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-19 14:58:46 -0600

    Mismo codigo que el codigo anterior. simplificado

--- js/java.js
+++ js/java.js
@@ -2,15 +2,12 @@ console.log("hello world")
 
 //ejecucion terniaria 
 
-let mensaje 
-const edad = 18
+//condicion ? true : false
 
-if (edad <= 8) {
-    mensaje = 'Es mayor de edad. Puede pasar'
-}else {
-    mensaje= "no es mayor de edad. No puede pasar."
-}
+//validar algo muy rapido, utilizamos la terminaria.
+//compleja con diferentes escenarios: else y else if
+
+const edad = 18 
+const mensaje = edad <= 18 ? "Es mayor de edad. Puede pasar" : "No es mayor de edad. No puede pasar"
 
 console.log(mensaje);
-
-
