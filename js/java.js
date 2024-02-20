abe46e8c3fe8fb50b73ef30ca7ab80ae49219fbc
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-19 23:37:00 -0600

    loop for

--- js/java.js
+++ js/java.js
@@ -2,10 +2,12 @@ console.log("hello world")
 
 //loop: for
 
-for (variable; condicion; incremento){
-    //codigo a ejecutar
-}//la condicion, siempre vendra de una variable externa 
 
-//si la condicion sucede, va aincrementar
-//cuando regreemos a la segunda vuelta, valdra mas. y entonces sucedera algo. 
-//se repetira infinitamente hasta que la condicion, se rompa.cuando ya no sea verdad, el codigo se deja de ejecutar. 
+let list = ["Eat", "Sleep", "Code", "Repeat"]
+
+for (let index = 0; index < list.length; index++) {
+
+    console.log(list[index]);
+}
+
+//ya le entendi al loop.
