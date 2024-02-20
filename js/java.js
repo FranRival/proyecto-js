55eb95925e8639a14f679a8da1f3028f43d99ec4
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-19 22:43:16 -0600

    ejemplo de switch

--- js/java.js
+++ js/java.js
@@ -1,13 +1,39 @@
 console.log("hello world")
 
-//ejecucion terniaria 
+//
+// Definición de una función llamada realizarTarea que toma un argumento llamado 'dia'
+function realizarTarea(dia) {
+    // Inicia la declaración switch, que evaluará el valor de 'dia'
+    switch (dia) {
+        // Si 'dia' es igual a cualquiera de estos casos ('lunes', 'martes', 'miércoles', 'jueves', 'viernes')
+        case 'lunes':
+        case 'martes':
+        case 'miércoles':
+        case 'jueves':
+        case 'viernes':
+            // Imprime en la consola que es un día laboral y se están realizando tareas laborales
+            console.log('Es un día laboral. Realizando tareas laborales.');
+            // Sale del bloque switch
+            break;
 
-//condicion ? true : false
+        // Si 'dia' es igual a 'sábado' o 'domingo'
+        case 'sábado':
+        case 'domingo':
+            // Imprime en la consola que es fin de semana y se está disfrutando del tiempo libre
+            console.log('Es fin de semana. Disfrutando del tiempo libre.');
+            // Sale del bloque switch
+            break;
 
-//validar algo muy rapido, utilizamos la terminaria.
-//compleja con diferentes escenarios: else y else if
+        // Si 'dia' no coincide con ninguno de los casos anteriores
+        default:
+            // Imprime en la consola que el día no es reconocido y sugiere verificar la entrada
+            console.log('Día no reconocido. Verifica la entrada.');
+    }
+}
 
-const edad = 18 
-const mensaje = edad <= 18 ? "Es mayor de edad. Puede pasar" : "No es mayor de edad. No puede pasar"
-
-console.log(mensaje);
+// Ejemplos de uso de la función realizarTarea con diferentes días como argumentos
+realizarTarea('lunes');     // Día laboral
+realizarTarea('sábado');    // Fin de semana
+realizarTarea('domingo');   // Fin de semana
+realizarTarea('miércoles'); // Día laboral
+realizarTarea('julio');     // Día no reconocido
