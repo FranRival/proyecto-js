bd8ef13761f921949d196ec7ae7063f08f3d3ebc
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-19 23:45:29 -0600

    un for each es como un swtich pero mas actual y para casos mas complejos

--- js/java.js
+++ js/java.js
@@ -2,7 +2,11 @@ console.log("hello world")
 
 //loop: foreach
 
-array.forEach(fruta => {
-    
-});//se va a ejecutar por cada elemento en la lista
-//por cada fruta que exista en la lista de de frutas, ejecuta este codigo. se va a repetir esta funcion cada uno de los elemtnos de la lista, hasta ya se hayan terminado los elemnto,s ygenera un break. entregando 
+
+let lista = ["Eat","Sleep","Code","Repeat"]
+
+lista.forEach((item) => {
+    console.log(item);
+})
+
+//simplificado. mismo resultado sin el switch. 
