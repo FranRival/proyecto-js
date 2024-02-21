76c02517bbddcfbae2873e45b203a201db65af4b
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-20 22:56:50 -0600

    while

--- js/java.js
+++ js/java.js
@@ -1,36 +1,22 @@
 console.log("hello world")
 
 //loop: 
-//for in: los objetos es una estructura de datos diferente a los arrays. los onbjetos tienen propiedades y las proiedades, un valor. 
-//si queremos iterar sobre estas propeidades, necesitmoas el for in
-
-// for in, objetos enumurables. 
-
-//objetos: propiedades = valor
-
-//arrays y string: elementos adentro que generan una lista. tiene una propiedad, y la proeidad un valor. 
+//while
 
 
-const listaDeCompras = {//objeto. propiedades basicas.
-    manzana: 5,
-    peras: 3,
-    naranja: 2,
-    uva: 1
-}
+//iterar: meter instrucciones en un ciclo, que se valla repitiendo, siempre y cuando sea true.
+
+/* 
+while(condicion){
+    //codigo. 
+}//loop infinito.  */
 
 
-//para iterar.
 
-for (const frutas in listaDeCompras) {
-    console.log(frutas);
-}
+let contador = 0
 
 
-//frutas es el indice 0 
-for (frutas in listaDeCompras){
-    console.log(`${frutas} : ${listaDeCompras[frutas]}`);
-}
-
-for (frutas of listaDeCompras){
-    console.log(frutas);//error. listaa de compras no es iterable. 
+while (contador < 10){
+    console.log(contador)
+    contador++
 }
