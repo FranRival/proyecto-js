f96dc3aa817592895b21a2f08102c703e00648e5
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-20 22:35:40 -0600

    estructura de for of

--- js/java.js
+++ js/java.js
@@ -1,12 +1,20 @@
 console.log("hello world")
 
-//loop: foreach
+//loop: 
+//for of: por cada elemento de cierta cosa
+//objetos iterables. arrays o strings. 
+
+for (const variable of objeto) {
+    //codigo
+}//una vez se rompe, ya no existe un objeto en la lista. y ya no se iterara
 
 
-let lista = ["Eat","Sleep","Code","Repeat"]
+let canasta = ["manzana", "pera", "naranja", "uva"]
 
-lista.forEach((item) => {
-    console.log(item);
-})
+for (const fruta of canasta) {
+    console.log(fruta);
+}
 
-//simplificado. mismo resultado sin el switch. 
+
+
+
