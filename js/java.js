c74412b14270b441032bb817b4bbd8293a0fb19e
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-20 23:21:57 -0600

    juego simple con iteraciones

--- js/java.js
+++ js/java.js
@@ -2,24 +2,42 @@ console.log("hello world")
 
 //loop: 
 //do while 
+let palabraOculta = "javascript"
+let intentos = 3
 
-/* do{
-    //codigo
-}while(condicion){
-    
-} */
+function verificarSuposicion (supocision, palabraOculta){
+
+    if(supocision.toLowerCase()===palabraOculta)//convierta todo en minusculas.
+    {
+        return true
+    }
+    return false
+}//con esta funcion verificamos si lo que escribio el usuario es correcto
 
 
-let contador = 0 
+function jugarAdivinaLaPalabra(){
+    alert("Bienvenido a jugar la palabra oculta")
+    alert("Tienes 3 intentos para adivinar la palabra")
+    alert("--pista--, la palabra oculta es un lenguje deprogramacion")
 
-do {
-    console.log(contador);
-    contador++
-}while(contador < 10){
+    while(intentos > 0){
+        let supocision = prompt("Adivina la palabra")
 
+        if(verificarSuposicion(supocision, palabraOculta)){
+            alert("COrrecto! Has advinado la keyword")
+            break
+        }else{
+            intentos--
+
+            if( intentos > 0){
+                alert(`Incorrecto, aun tienes ${intentos} restantes.`)
+            }else{
+                alert(`Agotaste tus intentos. La palabra oculta era ${palabraOculta}`)
+            }
+        }
+
+    }
 }
 
-//la diferencia: en un do while, primero se va a ejecutar un codigo, antes de validar la condicion.
 
-//while: validar la condicion, antes de ejecutar el codigo.
-
+jugarAdivinaLaPalabra()
