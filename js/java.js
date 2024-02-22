a27c0ca7a333d99f74fd202ea6f8374176350c38
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-21 22:47:37 -0600

    estructura de un array

--- js/java.js
+++ js/java.js
@@ -1,43 +1,31 @@
 console.log("hello world")
 
-//loop: 
-//do while 
-let palabraOculta = "javascript"
-let intentos = 3
+//array: mutabilidad e inmutabildad
 
-function verificarSuposicion (supocision, palabraOculta){
+//array: guardar muchos valores en una sola variable. y son objetos. 
 
-    if(supocision.toLowerCase()===palabraOculta)//convierta todo en minusculas.
-    {
-        return true
-    }
-    return false
-}//con esta funcion verificamos si lo que escribio el usuario es correcto
+//2 maneras de crearlo
+
+new Array()
+
+const frutas = Array("Apple", "platano", "naranja")
+
+console.log(frutas)
 
 
-function jugarAdivinaLaPalabra(){
-    alert("Bienvenido a jugar la palabra oculta")
-    alert("Tienes 3 intentos para adivinar la palabra")
-    alert("--pista--, la palabra oculta es un lenguje deprogramacion")
-
-    while(intentos > 0){
-        let supocision = prompt("Adivina la palabra")
-
-        if(verificarSuposicion(supocision, palabraOculta)){
-            alert("COrrecto! Has advinado la keyword")
-            break
-        }else{
-            intentos--
-
-            if( intentos > 0){
-                alert(`Incorrecto, aun tienes ${intentos} restantes.`)
-            }else{
-                alert(`Agotaste tus intentos. La palabra oculta era ${palabraOculta}`)
-            }
-        }
-
-    }
-}
+const justOneNumber = Array(12)
+console.log(justOneNumber)
 
 
-jugarAdivinaLaPalabra()
+const numbers = Array(1,2,3,4,5,6,7)
+console.log(numbers)
+
+
+//2: array literal sintax
+
+const oneNumer = [4]
+console.log(oneNumer)
+
+const emptyArray = []
+console.log(emptyArray) //vacios, cuando lo iremos llenando
+
