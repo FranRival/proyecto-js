d80abeb09b9ab17ba16608d6a63466c88f1e5a04
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-02-21 23:04:25 -0600

    ejemplo simple de sumar con array, mas for

--- js/java.js
+++ js/java.js
@@ -1,70 +1,19 @@
 console.log("hello world")
 
-//array: mutabilidad e inmutabildad
-
-//array: guardar muchos valores en una sola variable. y son objetos. 
-
-//2 maneras de crearlo
-
-new Array()
-
-const frutas = Array("Apple", "platano", "naranja")
-
-console.log(frutas)
+//array:
 
 
-const justOneNumber = Array(12)
-console.log(justOneNumber)
+//practical excersice
+//la suma de todos los elementos de un array
 
+const numberArray = [1, 2, 3, 4, 5]
+let sum = 0
 
-const numbers = Array(1,2,3,4,5,6,7)
-console.log(numbers)
+//sumar los numeros del arrays. iterar cada pocision.
 
+for (let i = 0;i < numberArray.length; i++){
+    sum += numberArray[i] //igual a la suma de numberArray, mas lo que teniamos anteriormente 
+}//una operacion simple.
 
-//2: array literal sintax
-
-const oneNumer = [4]
-console.log(oneNumer)
-
-const emptyArray = []
-console.log(emptyArray) //vacios, cuando lo iremos llenando
-
-const recipesIngredientes = [ //array mixto
-    'Four',
-    true,
-    2,{
-        Ingredientes: 'Milk', quantity: '1 cup'
-    },
-    false
-]
-
-console.log(recipesIngredientes)
-
-
-//accesing array elements
-
-const firstFruit = frutas[0]
-console.log(firstFruit) 
-
-//lengt de un array
-const numberOfFruits = frutas.length
-console.log(numberOfFruits)
-
-
-//mutabilidad
-frutas.push('Melon') //inyectar informacion
-console.log(frutas)
-
-
-//inmutabilidad
-const newFrutas = frutas.concat(['Peras', 'Kiwi'])
-console.log(newFrutas) //nuevo array 
-console.log(frutas) //array sin modificar
-
-//no todos los metodos modifican el array original 
-
-//verificar si un array es un array
-
-const isArray = Array.isArray(frutas)
-console.log(isArray) //saber si es un array. arroja un boleano. true o false. 
+console.log(sum)
 
