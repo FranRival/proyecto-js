36c400325343542b5bc1ca6b208ea784929fda9a
Fran <Fran-RivasWNER@outlook.es>
2024-02-24 23:10:58 -0600

    temperaturas ferenheit a celsius

--- js/java.js
+++ js/java.js
@@ -1,22 +1,10 @@
 
 //map() y foreach()
 
-// no modifican el array original.
+//excersice: farenheit y celcius 
 
-//permite colocar una funcion en cada elemento del array. 
+const temperaturasFarenheit = [32,87,44, 104,223]
+const temperaturasCelsius = temperaturasFarenheit.map(Farenheit => (5/9) * (Farenheit - 32))
 
-const numbers = [1,2,3,4,5]
-const squareNumbers = numbers.map(num => num * num)
-
-console.log(numbers)
-console.log(squareNumbers)
-
-
-//foreach: 
-
-const colors = ['red', 'pink', 'blue']
-//const iteratedColors = colors.forEach() //recorre cada una de las pocisiones 
-const iteratedColors = colors.forEach(color => console.log(color))
-
-console.log(colors)
-console.log(iteratedColors)
+console.log('Temperaturas en farenheit: ', temperaturasFarenheit)
+console.log('Temperaturas en grados celcius: ', temperaturasCelsius)
