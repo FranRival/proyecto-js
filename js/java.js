6dd81b0a46d279f2ec9e0ecec1906ce35c956aaf
Fran <Fran-RivasWNER@outlook.es>
2024-02-25 23:02:46 -0600

    este codigo no lo entendi. es el filter y el reduce.

--- js/java.js
+++ js/java.js
@@ -1,23 +1,43 @@
 
-//map() y foreach()
+//filter() y reduce()
 
-//excersice: farenheit y celcius 
+//filter crea un nuevo array con elementos que cumplen con una condicion dada por una funcion 
 
-const temperaturasFarenheit = [32,87,44, 104,223]
-const temperaturasCelsius = temperaturasFarenheit.map(Farenheit => (5/9) * (Farenheit - 32))
+const numeros = [1,2,34,54,10,29,91,73,32,8]
+const evenNumbers = numeros.filter(numbers => numbers % 2 === 0) //el filter pide una funcion dentro de los parentesis
 
-console.log('Temperaturas en farenheit: ', temperaturasFarenheit)
-console.log('Temperaturas en grados celcius: ', temperaturasCelsius)
 
-//sum of elements array - EJEMPLO
+//un modulo = % ---- 4|2
 
-const numbers = [1,2,3,4,5]
-let suma = 0
+console.log(numeros)
+console.log(evenNumbers)
 
-numbers.forEach(number => {
-  //  suma = suma + number
-    suma += number
-})
 
-console.log('array of numbers', numbers)
-console.log('Suma de los numeros ', suma);
+//reduce.
+//reduce un array a un solo valor. suma todos los valores obteniendo un solo valor. 
+
+
+const numbersReduce = [1,2,3,4,5]
+const sumar = numbersReduce.reduce((accumulator, currentValue)=> accumulator + currentValue, 0)//agregar valor por dentro. 2 parametros. 
+
+console.log('Este es el array de numeros: ', numbersReduce)
+console.log(sumar)
+
+//encontrar cuanto se repite una palabra en un array. 
+
+//reduce . caso 2
+
+
+const words = ['Apple', 'Banana', 'Barbie', 'Barbie', 'Apple']
+const reduceCaso2 = words.reduce((accumulator, currentValue)=> {
+  if (accumulator[currentValue]){
+    accumulator[currentValue]++
+  }else{
+    accumulator[currentValue] = 1
+
+  }
+  return accumulator
+}, {})
+
+console.log(reduceCaso2);
+
