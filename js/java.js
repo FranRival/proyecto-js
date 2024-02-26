d6c5f52c6181731d5fbdd0a01670445e5e39a13a
Fran <Fran-RivasWNER@outlook.es>
2024-02-25 23:11:24 -0600

    otro programa que utiliza la logica pura y dura. NOT UNDESTAND NOTHING.

--- js/java.js
+++ js/java.js
@@ -1,22 +1,17 @@
 
-//filter() y reduce()
-//Creamos la variable calificaciones las cuales les pasamos las calificaciones
-const calificaciones = [100, 90, 100, 80, 90, 80, 65, 28, 60, 45, 72, 55, 88, 40, 68, 75, 58, 95, 62, 50, 82, 35]
+//calificacion promedio aprobatoria
 
+//ejercicio
 
-//creamos una variable calificaciones pasanates en la cual  de decimos que a calificaciones le haga un filatrado de mayor o igual que 70 
-const calificacionesPasantes  = calificaciones.filter(calificacion => calificacion >= 70)
+//promedio para aprobar
 
+const grades = [85, 70, 83, 53, 24, 30, 29, 58, 60]
+const passigGrades = grades.filter(grade => grade >= 70)
 
-//Creamos una variable para el promedio en la cual  le decimos que a calificaciones pasantes las sume toda y despues la divida por la longitud del array y así nos de el promedio 
-const promedioDeCalificaciones = calificacionesPasantes.reduce((sum, calificacion) => sum + calificacion, 0) / calificacionesPasantes.length;
+const averagePassingGrade = passigGrades.reduce((suma, grade) => suma + grade, 0) / passigGrades.length
 
+console.log('Original grades: ', grades)
+console.log('Passing grades: ', passigGrades)
+console.log('Average passing grades: ', averagePassingGrade)
 
-//Como el promedio nos muestra decumales pasamos un removedor de decimales 
-const promedioSinDecimales = Math.floor(promedioDeCalificaciones);
-
-
-///Imprimimos  las 3 variables principales
-console.log(calificaciones)
-console.log(calificacionesPasantes)
-console.log(promedioSinDecimales)
+//otro programa que no understand. 
