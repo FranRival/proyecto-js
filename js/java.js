2f34d7597e15c138b0950b731eef8d3fc2eef8d5
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-03 23:44:45 -0600

    sumando el amount. Hay un detalle de programacion, en la linea 30. porque esta el acc sin un valor, y aun asi funciona el programa...

--- js/java.js
+++ js/java.js
@@ -1,54 +1,35 @@
 
+//iteraccion de array
+//analisis de transaccion 
 
-const listaParticipantesGanadores = [
-  {
-    id: 1,
-    nombre: 'Jennifer',
-    ticketNumber: 001,
+
+const transaccions = [
+  {id: 1,
+  description: 'Grocery',
+  amount: -50
   },
   {
     id: 2,
-    nombre: 'Michael',
-    ticketNumber: 015,
-  },
-  {
+    description: 'Salary Deposit',
+    amount: 2000
+   },
+   {
     id: 3,
-    nombre: 'Emily',
-    ticketNumber: 011, 
-  },
-  {
+    description: 'Utility Bill Payment',
+    amount: -100
+   },
+   {
     id: 4,
-    nombre: 'Charly',
-    ticketNumber: 111,
-  }
+    description: 'Online Purchase',
+    amount: -30
+   }
 ]
 
-function findWinnerByName (name){
+//calcular el total del balance
 
-  const winner = listaParticipantesGanadores.find(participants => participants.nombre === name) //el nombre del "participants.name" es igual al name que le estamos pasando por parametro....
-  //creo que la primero mierda de participants, es para que detecte que es una funcion 
-  return winner || 'No hay un ganad' 
-}
+const totalBalance = transaccions.reduce((acc, transaction) => acc + transaction.amount, 0) //el acc no esta definido. sooooooo, como carajos funciona this mofo?
 
-function findWinnerIndexByTicket (ticketNumber){
-
-  const index = listaParticipantesGanadores.findIndex(participants => participants.ticketNumber === ticketNumber)
-
-  return index !== -1 ? index: "No hay ganador con ese ticket" //diferente menos uno, arroja el index. sino, nos arroja el otro valor. 
-}
-
-//desplega la informacion. 
-function displayWinnerInformation (Ganaodres){
-  if (Ganaodres !== undefined && Ganaodres != null && Ganaodres !== "No hay un ganador.") {
-    console.log("Winner informations: ", Ganaodres)
-  }else {
-    console.log('No se encontro ganandor ');
-  }
-}
-
-const ganandorPorNombre = findWinnerByName('Emily') //aqui hay un error. deberia aparecer la informacion. 
-const indexWinnerByTicket = findWinnerIndexByTicket(015)
+console.log('El total del saldo: ', totalBalance)
 
 
-displayWinnerInformation(ganandorPorNombre)
-console.log('Index of winner by ticet number: ', indexWinnerByTicket);
+//2. find the largest transacction (income or expense)
