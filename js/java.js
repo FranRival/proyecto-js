38fff1b39d44d8ba7c76523567172abac64f81f5
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-06 23:25:29 -0600

    every y some

--- js/java.js
+++ js/java.js
@@ -1,44 +1,18 @@
 
-//uniendo metodos. COncat. 
-//no modifica el array
+//every y some
+const edades = [21, 25, 30, 19, 22, 31]
 
-const morseCOde1 = ['....', '---'] //H O
-const morseCOde2 = ['.-..', '.-'] // L A
-const morseCodeMessage = morseCOde1.concat(morseCOde2)
+//every
 
+const todosSonAdultos = edades.every( edades => edades > 18) //pide como parametro una funcion
 
-
-console.log(morseCOde1)
-console.log(morseCOde2)
-console.log(morseCodeMessage)
+console.log(edades)
+console.log(todosSonAdultos) //devuelve un true
 
 
-//combine with concat .2
+//some
 
-const morseCodeMessage1 = [].concat(morseCOde1, morseCOde2)//array vacio
+const alMenosUnoEnTreinta = edades.some(edades => edades > 30)
 
-console.log(morseCodeMessage1);
-
-
-//spread operatior, con 3 puntos
-
-function combineMorseMessages (morseCode1, morseCode2){
- console.log('Esta es la funcion de combinar mensaje',[...morseCode1, ...morseCode2])
-
-}//se combinara el array. 
-
-combineMorseMessages(morseCOde1,morseCOde2)
-console.log();
-
-
-const numbers = [1, 2, 3]
-const string = 'String'
-
-combineMorseMessages(numbers,string) //separados. siendo parte del array
-
-
-//JOIN. junta todos los datos, y devuelve un solo valor. 
-
-const morseCodeMessaString = morseCodeMessage1.join(' # ')
-
-console.log('Utilizando Join: ', morseCodeMessaString);
+console.log(edades)
+console.log(alMenosUnoEnTreinta)
