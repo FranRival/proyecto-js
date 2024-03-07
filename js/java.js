15e0467be0e4151125dc985e11a3b0cc8d394905
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-06 23:35:18 -0600

    lastindexof

--- js/java.js
+++ js/java.js
@@ -20,3 +20,11 @@ console.log(index1)
 const index2 = frutas.indexOf('peras')
 console.log(index2)
 
+//3. Lasindexof
+
+const numerosDeNuevo = [2,4,6,8,10,6]
+const ultimoIndice = numerosDeNuevo.lastIndexOf(8)
+console.log(ultimoIndice)
+
+const ultimoIndice1 = numerosDeNuevo.lastIndexOf(292)
+console.log(ultimoIndice1)//tambien sale -1
