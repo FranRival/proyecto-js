58c33b1766ad949126d52a92e3e6be1afdfb287f
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-06 23:31:20 -0600

    indexof

--- js/java.js
+++ js/java.js
@@ -8,3 +8,15 @@ const numeros = [1, 2, 3, 4, 5]
 const resultado1 = numeros.includes(3)
 console.log(resultado1) 
 //hay un numero que tenga este del parentesis. resultado, true o false. 
+
+//index of. retorna -1 si el resultado es negativo. no true.
+
+const frutas = ['manzana', 'cereza', 'uvas', 'mango']
+const index1 = frutas.indexOf('uvas')
+
+console.log(index1)
+
+//un elemento que no existe:
+const index2 = frutas.indexOf('peras')
+console.log(index2)
+
