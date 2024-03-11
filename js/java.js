525ef38fe0710cb0f95fd55634daf1fba378b257
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-10 23:12:08 -0600

    encontrando los indices, resolviendo el probnlema de elemento no definido.

--- js/java.js
+++ js/java.js
@@ -3,28 +3,26 @@
 //indexof
 //lastindexof
 
-const numeros = [1, 2, 3, 4, 5]
+const stringArray = ['manzanas', 'platanos', 'naranjas', 'uvas', 'platanos', 'kiwi']
 
-const resultado1 = numeros.includes(3)
-console.log(resultado1) 
-//hay un numero que tenga este del parentesis. resultado, true o false. 
+const objetivo = 'platanos'
 
-//index of. retorna -1 si el resultado es negativo. no true.
+function encuentraIndicesEnElArray (array, objetivo){
+    const result = {
+        includesTargetString: false,
+        fistOcurrenciaIndex: -1,
+        lastOcurrenciaIndex: -1,
+    }
+    array.forEach(element, index=>{
+        if (element.includes(objetivo)) {
+            result.includesTargetString = true
+            result.fistOcurrenciaIndex = array.indexOf(objetivo)
+            result.lastOcurrenciaIndex = array.lastIndexOf(objetivo)
+        }
+    })
+    return result
+}
 
-const frutas = ['manzana', 'cereza', 'uvas', 'mango']
-const index1 = frutas.indexOf('uvas')
+const resultado = encuentraIndicesEnElArray (stringArray, objetivo)
 
-console.log(index1)
-
-//un elemento que no existe:
-const index2 = frutas.indexOf('peras')
-console.log(index2)
-
-//3. Lasindexof
-
-const numerosDeNuevo = [2,4,6,8,10,6]
-const ultimoIndice = numerosDeNuevo.lastIndexOf(8)
-console.log(ultimoIndice)
-
-const ultimoIndice1 = numerosDeNuevo.lastIndexOf(292)
-console.log(ultimoIndice1)//tambien sale -1
+console.log(resultado)
