5aaaf86cad9b8c7102d84200451395067f6ccd3f
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-11 23:33:06 -0600

    2da parte. not understand a la dickenson.

--- js/java.js
+++ js/java.js
@@ -15,6 +15,7 @@ const resultados = [0,0,1]
 function ganadorTorneo (competidores, resultados){
 
     const calificaciones = {}
+    let ganadores = ''
 
     for (let i = 0; i < competidores.length; i++) {
      //   const home = competidores[i][0]
@@ -22,9 +23,17 @@ function ganadorTorneo (competidores, resultados){
         //home y way signifca, equipo del pueblo , y equipo extrangero alv
 
         const equipoGanador = resultados[i] === 0 ? away : home
+        calificaciones[equipoGanador] = (calificaciones[equipoGanador] || 0) + 3 
+        //si el valor no existe, asigna un valor 0. 
 
-        calificaciones[equipoGanador]
+   
+        if (!ganadores || calificaciones[equipoGanador] > calificaciones[ganadores]) {
+            ganadores = equipoGanador
+        }
     }
+    return ganadores
 }
 
 ganadorTorneo (competidores, resultados)
+
+console.log(ganadorTorneo (competidores, resultados))
