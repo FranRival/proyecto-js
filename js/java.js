ba0cb943f8717e5b36cbb98b73ae6caf7d968457
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-11 23:26:37 -0600

    instanciar un nulo y hacer que exista. linea 25

--- js/java.js
+++ js/java.js
@@ -13,12 +13,17 @@ const resultados = [0,0,1]
 
 
 function ganadorTorneo (competidores, resultados){
+
+    const calificaciones = {}
+
     for (let i = 0; i < competidores.length; i++) {
      //   const home = competidores[i][0]
         const [home, away] = competidores[i]//creando 2 variables. en la primera pocsion, se toma home, y la segunda away     
         //home y way signifca, equipo del pueblo , y equipo extrangero alv
 
         const equipoGanador = resultados[i] === 0 ? away : home
+
+        calificaciones[equipoGanador]
     }
 }
 
