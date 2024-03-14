7c410efc6209c7a5cd58d454ec1c8d2244160ab0
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-14 00:03:56 -0600

    definicion de objeto

--- js/java.js
+++ js/java.js
@@ -1,41 +1,15 @@
 
-//arrays bidimensionales - 67
+//objetos. 
+//estructura de datos. 
+//key / value. 
+//propiedad = valor de la propiedad. 
 
-//el ganador del torneo. 
+/* 
+objeto NOMBRE {
+    propiedad: valor de la propiedad,
+    propiedad: otro valor, 
+    propiedad: mas valores,
+} 
+*/
 
-const competidores = [
-    ['Java', 'C#'],
-    ['C#', 'Python'],
-    ['Python', 'Java']
-]
-
-const resultados = [0,0,1]
-
-
-function ganadorTorneo (competidores, resultados){
-
-    const calificaciones = {} //un array de bojetos vacio. 
-    let ganadores = ''
-
-    for (let i = 0; i < competidores.length; i++) {
-     //   const home = competidores[i][0]
-        const [home, away] = competidores[i]//creando 2 variables. en la primera pocsion, se toma home, y la segunda away     
-        //home y way signifca, equipo del pueblo , y equipo extrangero alv
-
-        //2 variables de array. 
-
-        const equipoGanador = resultados[i] === 0 ? away : home
-        calificaciones[equipoGanador] = (calificaciones[equipoGanador] || 0) + 3 
-        //si el valor no existe, asigna un valor 0. 
-
-   
-        if (!ganadores || calificaciones[equipoGanador] > calificaciones[ganadores]) {
-            ganadores = equipoGanador 
-        }
-    }
-    return ganadores
-}
-
-ganadorTorneo (competidores, resultados)
-
-console.log(ganadorTorneo (competidores, resultados))
+//abstraer la informacion. para llevarlas a la programacion. 
