22378fbcec2d8a56a3febd8b6b17f8079688daa8
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-13 23:57:24 -0600

    linea 27 y 28.

--- js/java.js
+++ js/java.js
@@ -14,13 +14,15 @@ const resultados = [0,0,1]
 
 function ganadorTorneo (competidores, resultados){
 
-    const calificaciones = {}
+    const calificaciones = {} //un array de bojetos vacio. 
     let ganadores = ''
 
     for (let i = 0; i < competidores.length; i++) {
      //   const home = competidores[i][0]
         const [home, away] = competidores[i]//creando 2 variables. en la primera pocsion, se toma home, y la segunda away     
         //home y way signifca, equipo del pueblo , y equipo extrangero alv
+
+        //2 variables de array. 
 
         const equipoGanador = resultados[i] === 0 ? away : home
         calificaciones[equipoGanador] = (calificaciones[equipoGanador] || 0) + 3 
