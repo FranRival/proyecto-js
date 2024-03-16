588a324ee27616ae0d13e0727d5439ba628ffd1d
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-15 23:17:12 -0600

    esto es un molde de una clase

--- js/java.js
+++ js/java.js
@@ -1,41 +1,18 @@
 
-//objetos. 
-//
+//clases.
 
-let perspna = {
-    nombre: 'Emily',
-    apellido: 'Lujuriosa',
-    edad: 35
-}
+//crear nuevos objetos.
 
+class Persona {
+    //metodo constructor.
+    constructor(nombre, edad){
+        this.nombre = nombre
+        this.edad = edad
+    }
 
-//creando 150 personas. 
-//funcion constructora.
+    //metodo
+    saludar(){
+        console.log(`Hola, mi nombre es ${this.nombre} y tengo ${this.edad} edad`)
+    }
+}//esto es un molde.
 
-function nombre_dela_funcion_constructora_de_objeto (nombre, apellido,edad){
-    this.nombre = nombre
-    this.apellido = apellido
-    this.edad = edad
-}
-
-const persona1 = new nombre_dela_funcion_constructora_de_objeto('Alison', 'Rios', 25)
-
-console.log(persona1)
-
-persona1.nacionalidad = 'huasteco' //darle una nueva propiedad. 
-
-const persona2 = new nombre_dela_funcion_constructora_de_objeto ('Melisa', 'Yaiden', 34) 
-
-console.log(persona2) // no se imprime la nacionalidad.
-
-//persona1.nacionalidad: solo existe para persona1, y no persona2.
-
-
-//generar una nueva propiead en la funcion constructora: 
-
-nombre_dela_funcion_constructora_de_objeto.prototype.saludar = function(){
-    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
-}
-
-persona1.saludar()
-persona2.saludar()
