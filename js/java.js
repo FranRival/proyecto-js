b4f7a72795b8ac0173ac620e553fe5fd231714df
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-15 23:12:12 -0600

    funcion constructura, agregar nuevos metodos, o propiedades para compartir con las nuevas instancias, nuevos datos.

--- js/java.js
+++ js/java.js
@@ -22,4 +22,20 @@ const persona1 = new nombre_dela_funcion_constructora_de_objeto('Alison', 'Rios'
 
 console.log(persona1)
 
-persona1.nacionalidad = 'huasteco' //darle una nueva propiedad
+persona1.nacionalidad = 'huasteco' //darle una nueva propiedad. 
+
+const persona2 = new nombre_dela_funcion_constructora_de_objeto ('Melisa', 'Yaiden', 34) 
+
+console.log(persona2) // no se imprime la nacionalidad.
+
+//persona1.nacionalidad: solo existe para persona1, y no persona2.
+
+
+//generar una nueva propiead en la funcion constructora: 
+
+nombre_dela_funcion_constructora_de_objeto.prototype.saludar = function(){
+    console.log(`Hola, me llamo ${this.nombre} ${this.apellido}`);
+}
+
+persona1.saludar()
+persona2.saludar()
