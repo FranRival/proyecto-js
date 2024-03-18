00951ed71f4033d480e46a1cdcbd513f6fbdb423
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-18 00:04:15 -0600

    entendiendo this

--- js/java.js
+++ js/java.js
@@ -1,82 +1,18 @@
 
-//prototipos y herencias.
+//this. palabra reservada. se trabaja con clases. hace referencia al objeto o l aclase. 
 
-//prototype. mismos metodos y rpopiedades. es algo que sucede automaticamente sin necesidad de programar.
-
-//funciona con funciones y clases. pero no las instancias. 
-
-class Animal {
-    constructor(nombre,tipos){
+class Persona {
+    constructor(nombre, edad){
         this.nombre = nombre
-        this.tipos = tipos
-    }
-    //metodo
-    emitirSonido(){
-        console.log('El animal emite un sonido. ')
-    }
-}
-
-class perro extends Animal{
-    constructor(nombre,tipo,raza){
-        super(nombre, tipo)
-        this.raza = raza
-    }
-    emitirSonido(){
-        console.log("El perro ladra");
-    }
-    correr(){
-        console.log(`${this.nombre} corre alegremente.`)
+        this.edad = edad
     }
 }
 
 
-const perro1 = new perro('Alexa', 'Perro', 'Siberiano')
+const persona1 = new Persona("Alison", 25)
 
-console.log(perro1)
+console.log(persona1)
 
-perro1.correr()
-perro1.emitirSonido()
-
-
-perro1.nuevoMetodo = function(){
-    console.log("Este es un metodo")
-}//esta linea de codigo estamos agregando un nuevo metodo solo a la instancia. 
-//necesitamos agregar ese metodo a la clase constructora o la clase padre (perro)
-
-
-//hay que inyectar la infroamcion sin afectar la clase perro.
-
-
-
-perro.prototype.segundoMetodo = function (){
-    console.log('Es otro nuevo metodo');
-} 
-
-
-///lo que son las estancias a la dickenson:
-//perro1.prototype es indefinido.
-//perro.prototype
-
-perro1.prototype.segundoMetodo = function(){
-    console.log()
-}//no funciona. 
-//agrgando un prootupo a una instancia. pero solo se crea con las clases y funciones constructoras. 
-
-//gerarquia:
-
-//cadena de prototypo:
-
-//perro analiza el "SegundoMetodo"
-//y se da cuenta que no lo tiene en su constructor. 
-//pero si lo tiene en su prototipo. por lo tanto se puede acceder a ello.
-
-
-
-//--el prototypo actual. 
-let currentPrototype = Object.getPrototypeOf(perro1);//al objeto global, dame le prototipo de perro 1.
-//perro1 no tiene una isntancia porque no tiene.
-//cual es el prototipo del padre. 
-  for (;currentPrototype !== null; currentPrototype = Object.getPrototypeOf(currentPrototype)) {
-    console.log(currentPrototype);
-  }
-
+persona1.nuevoMetodo = function (){
+    console.log(`Mi nombre es ${this.nombre}`)
+}
