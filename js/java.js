3fb74e45521669013b328b64e2974603f492bf6e
Emmanuel Ibarra V. <Fran-RivasWNER@outlook.es>
2024-03-18 23:37:13 -0600

    el primer punto ya fue completado

--- js/java.js
+++ js/java.js
@@ -3,13 +3,9 @@
 Requerimientos del reto:
 
 1. El usuario debe poder ingresar su usuario y contraseña.
-2. El sistema debe ser capaz de validar si el usuario y 
-contraseña son correctos.
-3. Si el usuario y la contraseña son correctos, el sistema
-debe mostrar un mensaje de bienvenida y mostrar el timeline
-del usuario.
-4. Si el usuario o la contraseña son incorrectos, el sistema
-debe mostrar un mensaje de error y no mostrar ningun timeline.
+2. El sistema debe ser capaz de validar si el usuario y contraseña son correctos.
+3. Si el usuario y la contraseña son correctos, el sistema debe mostrar un mensaje de bienvenida y mostrar el timeline del usuario.
+4. Si el usuario o la contraseña son incorrectos, el sistema debe mostrar un mensaje de error y no mostrar ningun timeline.
 */
 
 const usersDatabase = [
@@ -26,3 +22,30 @@ const usersDatabase = [
         password: '123'
     }
 ]
+
+
+const usersTimeline = [
+    {
+        username: "Pablo",
+        timeline: "Este es el timeline de Pablo"
+    },
+    {
+        username: "Juan",
+        timeline: "Este es el timeline de Juan"
+    },
+    {
+        username: "Pedro",
+        timeline: "Este es el timeline de Pedro"
+    },
+    {
+        username: "Diego",
+        timeline: "Este es el timeline de Diego"
+    }
+]
+
+
+
+const username = prompt('CUal es tu usuario?')
+const pass = prompt('Cual es tu contrasena?')
+
+ 
